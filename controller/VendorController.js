31cd8ba9eb39b30ad55ff38ba66896dee3d7f920
NaveenBupati <156638825+NaveenBupati@users.noreply.github.com>
2024-05-17 12:49:08 +0530

    Update VendorController.js

--- controller/VendorController.js
+++ controller/VendorController.js
@@ -1,98 +1,17 @@
-const vendorSchema = require("../model/Vendor.js")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-
-const vendorRegistration = async(req,res) => {
-    try{
-
-        const {userName,email,password} = req.body
-
-        const exists  = await vendorSchema.findOne({email})
-
-        if(exists){
-            return res.status(400).json({message:"User already registered.."})
-        }
-
-        let hashedPassword = await bcrypt.hash(password,10);
-
-        const savedVendor = new vendorSchema({
-            userName,
-            email,
-            password:hashedPassword
-        })
-
-        const response = await savedVendor.save();
-        res.status(200).json({message:"User registered sucessfully..",response})
-    }
-    catch(err){
-        console.log(err)
-        res.status(500).status("Internal server error..")
-    }
-}
+const express = require("express")
+const router = express.Router();
+const firmController = require("../controller/firmController.js")
+const authToken = require("../middleware.js")
 
 
-const getDetails = async(req,res)=>{
-    try{
 
-        const details = await vendorSchema.find().populate("firm");
-        if(!details){
-            return res.status(404).json({message:"No user found"})
-        }
+router.post("/addfirm",authToken,firmController.addFirm)
 
-        res.status(200).json(details)
+router.get("/uploads/:imageName",(req, res)=>{
+    const imageName = req.params.imageName;
+    res.headersSent("Content-Type","image/jpeg");
+    res.sendFile(path.join(__dirname,".." , "uploads",imageName))
+})
 
-    }catch(err){
-        console.log(err)
-        res.status(500).status("Internal server error..")
-    }
-}
 
-const singleVendorDetails = async(req,res)=>{
-    const vendorId = req.params.id
-    try{
-
-        const vendor = await vendorSchema.findById(vendorId).populate("firm")
-        if(!vendor){
-            return res.ststus(404).json({message:"Vendor not found!.."})
-        }
-        res.status(200).json({vendor})
-    }
-    catch(err){
-        console.log(err)
-        res.status(500).json({message:"Internal server error..."})
-    }
-}
-
-const login = async(req,res)=>{
-    try{
-
-        const{email,password} = req.body;
-
-        let exists = await vendorSchema.findOne({email})
-        if(!exists){
-            return res.status(404).json({message:"No user found.."})
-        }
-
-        let passwordMatch = await bcrypt.compare(password,exists.password)
-        if(!passwordMatch){
-            return res.ststua(400).json({message:"Invalid credentials.."})
-        }
-
-        let payload = {
-            user:{
-                id:exists.id
-            }
-        }
-
-        jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:3600},(err,token)=>{
-            if(err) throw err;
-            return res.json({user:exists,token:token})
-        })
-
-    }catch(err){
-        console.log(err)
-        res.status(500).status("Internal server error..")
-    }
-}
-
-module.exports = {vendorRegistration,getDetails,login,singleVendorDetails}
+module.exports = router
