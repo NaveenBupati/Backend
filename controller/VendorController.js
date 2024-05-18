f85eea454b67fae803e92af40ae7c618413ec59f
NaveenBupati <156638825+NaveenBupati@users.noreply.github.com>
2024-05-18 15:17:18 +0530

    Update VendorController.js

--- controller/VendorController.js
+++ controller/VendorController.js
@@ -1,60 +1,105 @@
+const vendorSchema = require("../model/Vendor.js")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
-const firmModel = require("../model/Firm.js")
-const Vendor = require("../model/Vendor.js")
-const multer = require("multer")
-const path = require("path")
+const vendorRegistration = async(req,res) => {
+    try{
 
+        const {userName,email,password} = req.body
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/'); // Destination folder where the uploaded images will be stored
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // Generating a unique filename
-    }
-});
+        const exists  = await vendorSchema.findOne({email})
 
-const upload = multer({ storage: storage });
-
-  const addFirm = async (req, res) => {
-    try {
-        console.log("Vendor ID:", req.vendorId.user.id);
-        const { firmName, area, category, region, offer } = req.body;
-
-        const image = req.file ? req.file.filename : undefined;
-        console.log(image)
-
-        const vendor = await Vendor.findById(req.vendorId.user.id);
-
-        if (!vendor) {
-            return res.status(404).json({ message: "Vendor not found" });
-        }
-        if (vendor.firm.length > 0) {
-            return res.status(400).json({ message: "vendor can have only one firm" });
+        if(exists){
+            return res.status(400).json({message:"User already registered.."})
         }
 
-        const firm = new firmModel({
-            firmName,
-            area,
-            category,
-            region,
-            offer,
-            image,
-            vendor : vendor.id
-        });
+        let hashedPassword = await bcrypt.hash(password,10);
 
-        const savedFirm = await firm.save();
-        const firmId = savedFirm._id
-        vendor.firm.push(savedFirm);
-        await vendor.save();
+        const savedVendor = new vendorSchema({
+            userName,
+            email,
+            password:hashedPassword
+        })
 
-        res.status(200).json({ message: "Firm added successfully", firmId });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Internal server error" });
+        const response = await savedVendor.save();
+        res.status(200).json({message:"User registered sucessfully..",response})
     }
-};
+    catch(err){
+        console.log(err)
+        res.status(500).status("Internal server error..")
+    }
+}
 
- 
 
-module.exports = {addFirm : [upload.single("image"),addFirm]}
+const getDetails = async(req,res)=>{
+    try{
+
+        const details = await vendorSchema.find().populate("firm");
+        if(!details){
+            return res.status(404).json({message:"No user found"})
+        }
+
+        res.status(200).json(details)
+
+    }catch(err){
+        console.log(err)
+        res.status(500).status("Internal server error..")
+    }
+}
+
+const singleVendorDetails = async(req,res)=>{
+    const vendorId = req.params.id
+    try{
+
+        const vendor = await vendorSchema.findById(vendorId).populate("firm")
+        if(!vendor){
+            return res.ststus(404).json({message:"Vendor not found!.."})
+        }
+
+        const vendorFirmId =  vendor.firm[0].id
+        res.status(200).json({vendorId, vendorFirmId, vendor })
+        console.log(vendorFirmId);
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({message:"Internal server error..."})
+    }
+}
+
+const login = async(req,res)=>{
+    try{
+
+        const{email,password} = req.body;
+
+        let exists = await vendorSchema.findOne({email}).populate("firm")
+        if(!exists){
+            return res.status(404).json({message:"No user found.."})
+        }
+
+        let passwordMatch = await bcrypt.compare(password,exists.password)
+        if(!passwordMatch){
+            return res.ststua(400).json({message:"Invalid credentials.."})
+        }
+
+        let payload = {
+            user:{
+                id:exists.id
+            }
+        }
+
+        const expiresIn = 3600;
+
+        jwt.sign(payload,process.env.SECRET_KEY,{expiresIn},(err,token)=>{
+            if(err) throw err;
+            return res.json({user:exists,token:token, tokenExpired:expiresIn , vendorId:exists.id})
+        })
+
+        
+
+    }catch(err){
+        console.log(err)
+        res.status(500).status("Internal server error..")
+    }
+}
+
+module.exports = {vendorRegistration,getDetails,login,singleVendorDetails}
